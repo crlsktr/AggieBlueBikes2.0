8a739e9597d4a2fd925c2fc24cfbaf5e661455f3
crlsktr <crlsktr@gmail.com>
2017-11-17 21:53:22 -0700

    Calendar, menu, and feed

--- AggieBlueBikes3/app/src/main/java/com/example/tylerivory/aggiebluebikes/ABBFeedActivity.java
+++ AggieBlueBikes3/app/src/main/java/com/example/tylerivory/aggiebluebikes/ABBFeedActivity.java
@@ -3,7 +3,9 @@ package com.example.tylerivory.aggiebluebikes;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by crlsktr on 11/17/2017.
@@ -16,7 +18,10 @@ public class ABBFeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abb_feed);
         WebView feed = (WebView)findViewById(R.id.ABBfeedWebView);
-        String feedhtml = "<iframe src=\"https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Faggiebluebikes&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId\" width=\"340\" height=\"500\" style=\"border:none;overflow:hidden\" scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\"></iframe>";
+        feed.setWebViewClient(new WebViewClient());
+        feed.getSettings().setJavaScriptEnabled(true);
+        //feed.loadUrl("https://touch.facebook.com/AggieBlueBikes");
+        String feedhtml = "<iframe src=\"https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Faggiebluebikes&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId\" width=\"parent\" height=\"100%\" style=\"border:none;overflow:hidden\" scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\"></iframe>";
         feed.loadData(feedhtml,"text/html",null);
     }
 }
--- AggieBlueBikes3/app/src/main/java/com/example/tylerivory/aggiebluebikes/MainActivity.java
+++ AggieBlueBikes3/app/src/main/java/com/example/tylerivory/aggiebluebikes/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tylerivory.aggiebluebikes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +16,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void OpenFeed(View view) {
+    public void OpenFeed() {
         Intent intent = new Intent(this, ABBFeedActivity.class);
         startActivity(intent);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.abb_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.ABB_menuItem_feed:
+                OpenFeed();
+                return true;
+            case R.id.ABB_menuItem_help:
+                //showHelp();
+                return true;
+            case R.id.ABB_menuItem_checkout:
+                OpenCheckout();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void OpenCheckout() {
+        Intent intent = new Intent(this, ABBCheckoutActivity.class);
+        startActivity(intent);
     }
 }
